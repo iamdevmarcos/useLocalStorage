9f49da703e3d3b8043ad33da97cd146007013681
Marcos Mendes <marcosdev.me@gmail.com>
2023-01-11 19:57:35 -0300

    refactor: use isServerSide instead isClientSide

--- src/useLocalStorage.tsx
+++ src/useLocalStorage.tsx
@@ -1,28 +1,28 @@
 export const useLocalStorage = () => {
   const getStorageItem = (key: string) => {
-    const isClientSide = typeof window === 'object';
-    if (!isClientSide) return;
+    const isServerSide = typeof window === 'undefined';
+    if (!isServerSide) return;
 
     return window.localStorage.getItem(key)!;
   };
 
   const setStorageItem = (key: string, value: string) => {
-    const isClientSide = typeof window === 'object';
-    if (!isClientSide) return;
+    const isServerSide = typeof window === 'undefined';
+    if (!isServerSide) return;
 
     return window.localStorage.setItem(key, value);
   };
 
   const removeStorageItem = (key: string) => {
-    const isClientSide = typeof window === 'object';
-    if (!isClientSide) return;
+    const isServerSide = typeof window === 'undefined';
+    if (!isServerSide) return;
 
     return window.localStorage.removeItem(key);
   };
 
   const clearStorage = () => {
-    const isClientSide = typeof window === 'object';
-    if (!isClientSide) return;
+    const isServerSide = typeof window === 'undefined';
+    if (!isServerSide) return;
 
     return window.localStorage.clear();
   };
