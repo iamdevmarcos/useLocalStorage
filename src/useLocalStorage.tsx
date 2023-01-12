2f82e12a8350780836cbb5e959ef58264451cf3a
Marcos Mendes <marcosdev.me@gmail.com>
2023-01-12 05:41:28 -0300

    fix: remove negation

--- src/useLocalStorage.tsx
+++ src/useLocalStorage.tsx
@@ -1,28 +1,28 @@
 export const useLocalStorage = () => {
   const getStorageItem = (key: string) => {
     const isServerSide = typeof window === 'undefined';
-    if (!isServerSide) return;
+    if (isServerSide) return;
 
     return window.localStorage.getItem(key)!;
   };
 
   const setStorageItem = (key: string, value: string) => {
     const isServerSide = typeof window === 'undefined';
-    if (!isServerSide) return;
+    if (isServerSide) return;
 
     return window.localStorage.setItem(key, value);
   };
 
   const removeStorageItem = (key: string) => {
     const isServerSide = typeof window === 'undefined';
-    if (!isServerSide) return;
+    if (isServerSide) return;
 
     return window.localStorage.removeItem(key);
   };
 
   const clearStorage = () => {
     const isServerSide = typeof window === 'undefined';
-    if (!isServerSide) return;
+    if (isServerSide) return;
 
     return window.localStorage.clear();
   };
